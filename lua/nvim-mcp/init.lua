mcoffin/nvim-mcp

commit 3d1729eff5e123256494dc172a96bb8ae9dc0860

make coket accessible from within non-root containers

lua/nvim-mcp/init.lua
@@ -1,3 +1,4 @@
+local Job = require('plenary.job')
 local M = {}
 
 local has_setup = false
@@ -7,6 +8,8 @@ M._tool_registry = {}
 
 ---@class SetupOptions
 ---@field custom_tools table<string, CustomTool>|nil Custom tools configuration
+---@field socket_permissions string|nil Custom permissions bit mask for the RPC
+---socket (i.e. 777)
 
 ---@class CustomTool
 ---@field description string Tool description
@@ -161,7 +164,14 @@ function M.setup(opts)
 	end
 
     -- Start Neovim RPC server on the pipe
-    table.insert(M._sockets, vim.fn.serverstart(pipe_path))
+	local sock = vim.fn.serverstart(pipe_path)
+	local perms = M.socket_permissions or "777"
+	Job:new({
+		command = "chmod",
+		args = { perms, sock },
+		cwd = vim.fn.stdpath("run"),
+	}):sync()
+    table.insert(M._sockets, vim.fn.serverstart("nvim-mcp.*.sock"))
 end
 
 -- Tool Discovery API for MCP Server

commit 29d05e3b8ce6e53d0eba325db8f26edaf0c5c86d

Fix integration test regression due to lua dependency
Fixes: 3d1729eff5e123256494dc172a96bb8ae9dc0860

lua/nvim-mcp/init.lua
@@ -1,4 +1,3 @@
-local Job = require('plenary.job')
 local M = {}
 
 local has_setup = false
@@ -166,11 +165,14 @@ function M.setup(opts)
     -- Start Neovim RPC server on the pipe
 	local sock = vim.fn.serverstart(pipe_path)
 	local perms = M.socket_permissions or "777"
-	Job:new({
-		command = "chmod",
-		args = { perms, sock },
-		cwd = vim.fn.stdpath("run"),
-	}):sync()
+	pcall(function()
+		local Job = require('plenary.job')
+		Job:new({
+			command = "chmod",
+			args = { perms, sock },
+			cwd = vim.fn.stdpath("run"),
+		}):sync()
+	end)
     table.insert(M._sockets, sock)
 end
 